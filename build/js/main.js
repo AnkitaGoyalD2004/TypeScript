873e5f3ea380ce391184d43810b45dd59c7ee341
Ankita Goyal <goyalankita0325@gmail.com>
2024-07-29 14:10:10 +0530

    done with basics

---
+++ build/js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+// let username = "Ankita"
+// console.log(username)
+// let a: number = 12
+// let b: number = 6
+// let c: number = 2;
+// console.log(a / b);
+// console.log(c*b)
+let myName;
+let meaningOfLife;
+let isLoading;
+let album; // any is data type
+myName = "Ankita";
+meaningOfLife = 42;
+isLoading = true;
+album = 1245;
+album = "Dishu";
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(7, "Dishu"));
+// there is one more type which is union
+let success;
+let postId;
+let isActive;
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-let username = "Ankita"
-console.log(username)
+// let username = "Ankita"
+// console.log(username)
+
+// let a: number = 12
+// let b: number = 6
+// let c: number = 2;
+
+// console.log(a / b);
+// console.log(c*b)
+
+let myName: string;
+let meaningOfLife: number;
+let isLoading : boolean;
+let album: any; // any is data type
+
+myName = "Ankita"
+meaningOfLife = 42;
+isLoading = true;
+album = 1245
+album = "Dishu"
+const sum = (a:number ,b: string) =>{
+    return a + b;
+}
+console.log(sum(7,"Dishu"));
+
+// there is one more type which is union
+let success : boolean | number ;
+
+let postId : string | number;
+let isActive : number | boolean;
