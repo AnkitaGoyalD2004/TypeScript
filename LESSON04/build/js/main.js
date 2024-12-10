9029ae8aa9e06adda886dbd7329d7e0ef7634d62
Ankita Goyal <goyalankita0325@gmail.com>
2024-12-10 17:49:20 +0530

    about functions

---
+++ LESSON04/build/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Type ALIASES
+//In TypeScript, type aliases allow you to define a custom name for a type. 
+//They are a way to give a meaningful name to a type, making your code more readable and maintainable.
+// interface PostId = stringOrNumber -> this is not correct way
+//Literal type
+let myName;
+let username;
+username = 'Tashu';
+//functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello');
+logMsg(add(2, 3));
---
+++ LESSON04/src/main.ts
@@ -0,0 +1,34 @@
+// Type ALIASES
+//In TypeScript, type aliases allow you to define a custom name for a type. 
+//They are a way to give a meaningful name to a type, making your code more readable and maintainable.
+
+type stringOrNumber = string | number
+
+type stringOrNumberArray = (string | number )[]
+
+type Guitarist = {
+    name?: string,
+    active: boolean,
+    albums: stringOrNumberArray
+}
+
+type UserId = stringOrNumber
+
+// interface PostId = stringOrNumber -> this is not correct way
+
+//Literal type
+let myName : 'Ankita';
+let username : 'Ankita' | 'Kanav' | 'Tashu'
+username = 'Tashu'
+
+//functions
+const add = (a:number , b:number): number=>{
+    return a + b
+}
+
+const logMsg = (message : any): void =>{
+    console.log(message);
+    
+}
+logMsg('Hello')
+logMsg(add(2,3))
