c2163a9e336e70c3c09baef4449499b2229c6f29
Ankita Goyal <goyalankita0325@gmail.com>
2024-12-13 10:06:27 +0530

    about assertions

--- LESSON05/build/js/main.js
+++ LESSON05/build/js/main.js
@@ -14,3 +14,17 @@ const addOrConcat = (a, b, c) => {
 let myVal = addOrConcat(2, 2, 'concat');
 //Be careful! TS sees no problem - but a string is returned
 let nextVal = addOrConcat(2, 2, 'concat');
+// 10 as string
+10;
+/*
+Assertions in TypeScript are necessary because,
+in certain scenarios, the compiler cannot make accurate assumptions about the types of
+variables or expressions due to insufficient information.Assertions provide a way for
+developers to explicitly specify the type or guarantee certain conditions, ensuring that the code is
+understood and behaves as expected.
+*/
+//The Dom
+const img = document.getElementById('img');
+const myImg = document.getElementById('#img');
+img.src;
+// myImg.src
--- LESSON05/src/main.ts
+++ LESSON05/src/main.ts
@@ -1,23 +1,42 @@
-type One = string 
+type One = string
 type Two = string | number
 type Three = 'hello'
 
 // convert to more or less soecific
-let a: One  = 'hello'
+let a: One = 'hello'
 let b = a as Two // less specific
 let c = a as Three // more specific
 
 let d = <One>'world'
 let e = <string | number>'world'
 
-const addOrConcat = (a: number , b :number , c: 'add' | 'concat'): number | string =>{
-    if(c === 'add'){
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
+    if (c === 'add') {
         return a + b
     }
     return '' + a + b
 }
 
-let myVal: string = addOrConcat(2,2,'concat') as string
+let myVal: string = addOrConcat(2, 2, 'concat') as string
 
 //Be careful! TS sees no problem - but a string is returned
-let nextVal:number = addOrConcat(2,2,'concat') as number
+let nextVal: number = addOrConcat(2, 2, 'concat') as number
+
+// 10 as string
+(10 as unknown) as string
+
+/*
+Assertions in TypeScript are necessary because, 
+in certain scenarios, the compiler cannot make accurate assumptions about the types of 
+variables or expressions due to insufficient information.Assertions provide a way for 
+developers to explicitly specify the type or guarantee certain conditions, ensuring that the code is 
+understood and behaves as expected. 
+*/
+
+
+//The Dom
+const img = document.getElementById('img') as HTMLImageElement 
+const myImg = document.getElementById('#img');
+
+img.src
+// myImg.src
