a15c7d4712c63c5e87892ada58af8c2c08019816
Ankita Goyal <goyalankita0325@gmail.com>
2024-12-14 16:35:54 +0530

    about classes

--- LESSON06/build/js/main.js
+++ LESSON06/build/js/main.js
@@ -1,6 +1,17 @@
 "use strict";
 class coder {
-    constructor(name) {
+    // name: string
+    // music: string 
+    // age: number
+    // lang: string
+    constructor(name, music, age, lang) {
         this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
     }
 }
--- LESSON06/src/main.ts
+++ LESSON06/src/main.ts
@@ -1,7 +1,19 @@
 class coder{
-    name: string
+    // name: string
+    // music: string 
+    // age: number
+    // lang: string
 
-    constructor(name: string){
+    constructor(
+        public readonly name:string,
+        public music: string,
+        private age:number,
+        protected lang:string
+    ){
         this.name = name
+        this.music = music
+        this.age = age
+        this.lang = lang
+
     }
 }
