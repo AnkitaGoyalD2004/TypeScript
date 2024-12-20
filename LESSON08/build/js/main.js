2f591cd44c0eae1864ef5a9f23bb09e43814ecc6
Ankita Goyal <goyalankita0325@gmail.com>
2024-12-20 17:20:51 +0530

    about Generics

--- LESSON08/build/js/main.js
+++ LESSON08/build/js/main.js
@@ -27,3 +27,78 @@ const checkBoolValue = (arg) => {
     }
     return { value: arg, is: !!arg };
 };
+const processUser = (user) => {
+    // process the user with logic here
+    return user;
+};
+console.log(processUser({ id: 1, name: '' }));
+const getUsersProperty = (users, key) => {
+    return users.map(user => user[key]);
+};
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+];
+console.log(getUsersProperty(usersArray, "email"));
+console.log(getUsersProperty(usersArray, "username"));
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const store = new StateObject("Dutt");
+console.log(store.state);
+store.state = "Ankita";
+const myState = new StateObject([15]);
+myState.state = (['Tashu', 78, true]);
+console.log(myState.state);
--- LESSON08/src/main.ts
+++ LESSON08/src/main.ts
@@ -36,3 +36,93 @@ const checkBoolValue = <T>(arg : T): BoolCheck<T> => {
    }
    return {value :  arg , is : !!arg}
 }
+
+interface HasID {
+   id: number
+}
+
+const processUser = <T extends HasID>(user : T): T =>{
+   // process the user with logic here
+   return user
+}
+
+console.log(processUser({id: 1 , name: ''}));
+
+const getUsersProperty = <T extends HasID , K extends keyof T> (users : T[] , key : K) : T[K][] => {
+   return users.map(user => user[key])
+}
+
+const usersArray = [
+   {
+       "id": 1,
+       "name": "Leanne Graham",
+       "username": "Bret",
+       "email": "Sincere@april.biz",
+       "address": {
+           "street": "Kulas Light",
+           "suite": "Apt. 556",
+           "city": "Gwenborough",
+           "zipcode": "92998-3874",
+           "geo": {
+               "lat": "-37.3159",
+               "lng": "81.1496"
+           }
+       },
+       "phone": "1-770-736-8031 x56442",
+       "website": "hildegard.org",
+       "company": {
+           "name": "Romaguera-Crona",
+           "catchPhrase": "Multi-layered client-server neural-net",
+           "bs": "harness real-time e-markets"
+       }
+   },
+   {
+       "id": 2,
+       "name": "Ervin Howell",
+       "username": "Antonette",
+       "email": "Shanna@melissa.tv",
+       "address": {
+           "street": "Victor Plains",
+           "suite": "Suite 879",
+           "city": "Wisokyburgh",
+           "zipcode": "90566-7771",
+           "geo": {
+               "lat": "-43.9509",
+               "lng": "-34.4618"
+           }
+       },
+       "phone": "010-692-6593 x09125",
+       "website": "anastasia.net",
+       "company": {
+           "name": "Deckow-Crist",
+           "catchPhrase": "Proactive didactic contingency",
+           "bs": "synergize scalable supply-chains"
+       }
+   },
+]
+
+console.log(getUsersProperty(usersArray, "email"))
+console.log(getUsersProperty(usersArray, "username"))
+
+class StateObject<T> {
+   private data : T
+
+   constructor(value : T){
+      this.data = value
+   }
+   get state() : T{
+      return this.data
+   }
+   set state(value:T){
+      this.data = value
+   }
+}
+
+const store = new StateObject("Dutt")
+console.log(store.state);
+store.state = "Ankita"
+
+const myState = new StateObject<(string | number | boolean)[]>([15])
+   myState.state = (['Tashu' , 78 , true])
+   console.log(myState.state);
+   
