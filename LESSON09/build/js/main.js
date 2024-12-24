05d10e22492e60197bb6639f0dc9ab9b431c90a7
Ankita Goyal <goyalankita0325@gmail.com>
2024-12-24 15:57:52 +0530

    about utility types

--- LESSON09/build/js/main.js
+++ LESSON09/build/js/main.js
@@ -3,3 +3,18 @@
 const updateAssignment = (assign, propsToUpdate) => {
     return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
+const assign1 = {
+    studentId: 'compsci123',
+    title: "Final Project",
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
+// /Required and read only
+const recordAssignment = (assign) => {
+    //send to database
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// assignVerified.grade = 88 // we cannot assign it because its readonly
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
--- LESSON09/src/main.ts
+++ LESSON09/src/main.ts
@@ -12,3 +12,27 @@ interface Assignment {
 const updateAssignment = (assign : Assignment , propsToUpdate : Partial<Assignment>): Assignment => {
    return {...assign , ...propsToUpdate}
 }
+
+const assign1 : Assignment = {
+   studentId: 'compsci123' , 
+   title: "Final Project",
+   grade: 0,
+}
+
+console.log(updateAssignment(assign1 , {grade : 95}))
+const assignGraded : Assignment = updateAssignment(assign1 , {grade : 95})
+
+// /Required and read only
+
+const recordAssignment = (assign : Required<Assignment>): Assignment =>{
+   //send to database
+   return assign
+}
+
+const assignVerified : Readonly<Assignment> = {
+   ...assignGraded , verified: true
+}
+
+// assignVerified.grade = 88 // we cannot assign it because its readonly
+
+recordAssignment({...assignGraded , verified: true})
