abdbfed9a8360982f9661ea8d7e50ca20ab78268
Ankita Goyal <goyalankita0325@gmail.com>
2025-01-10 19:11:39 +0530

    done with useContext

--- LESSON15/src/App.tsx
+++ LESSON15/src/App.tsx
@@ -1,12 +1,19 @@
+import { CounterProvider, initState } from "./context/CounterContext"
 import Counter from "./Counter"
 
 function App() {
 
   return (
     <>
+    <CounterProvider count={initState.count} text={initState.text}>
       <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      </CounterProvider>
     </>
   )
 }
 
 export default App
+
+
+// The Provider only expects one child . Wrap the children in a fragment <> </> OR change the ChildrenType in context to:
+// ReactElement | ReactElement[] | undefined
